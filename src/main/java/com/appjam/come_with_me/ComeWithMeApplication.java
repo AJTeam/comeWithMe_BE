e0a76996fe6db1a57510853ff7cfbc6166edd26e
CSeungJoo <seungjoo13579@gmail.com>
2024-06-22 16:55:31 +0900

    init

---
+++ src/main/java/com/appjam/come_with_me/ComeWithMeApplication.java
@@ -0,0 +1,13 @@
+package com.appjam.come_with_me;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ComeWithMeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ComeWithMeApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/appjam/come_with_me/ComeWithMeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.appjam.come_with_me;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ComeWithMeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
